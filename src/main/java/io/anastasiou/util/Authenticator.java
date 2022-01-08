0460685ed8f48440b179d666082bfb42a2540b16
Loukas Anastasiou <loukas@anastasiou.io>
2022-01-08 12:24:22 +0100

    Removed import

--- src/main/java/io/anastasiou/util/Authenticator.java
+++ src/main/java/io/anastasiou/util/Authenticator.java
@@ -2,8 +2,6 @@ package io.anastasiou.util;
 
 import io.anastasiou.UnauthorizedException;
 
-import javax.naming.AuthenticationException;
-
 @FunctionalInterface
 public interface Authenticator {
     void login() throws UnauthorizedException;
